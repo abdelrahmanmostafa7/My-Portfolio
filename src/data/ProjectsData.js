3f3bd6471d944ce380434382cbdc1e43e54037f8
boyka <abdelrahmanzaki747@gmail.com>
2025-05-12 17:35:15 +0300

    some design edit

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,27 +1,11 @@
 const ProjectsData = [
   {
-    id: 1,
-    title: "Full Stack E-Commerce",
-    img: "/assets/ecommerce.png",
-    desc: "Developed a full-stack e-commerce platform with a responsive React frontend and a Node.js/Express backend. Implemented features like user authentication, product management, shopping cart functionality, and order processing. Utilized MongoDB for data storage and Redux for state management, ensuring a seamless user experience.",
-    githup: "https://github.com/abdelrahmanmostafa7/Full-Stack-E-Commerce",
-    liveDemo: "https://full-stack-e-commerce-neon.vercel.app/",
-  },
-  {
-    id: 4,
-    title: "Tour Scope",
-    img: "/assets/tourScope.png",
-    desc: "A full-stack MERN application designed for personalized travel planning. Features include AI-powered hotel and city recommendations, an interactive Mapbox map for exploration, responsive carousels for showcasing hotels, and a user-friendly interface. Integrated with secure authentication, dynamic data fetching, and a scalable backend API to deliver a seamless travel experience.",
-    githup: "https://github.com/abdelrahmanmostafa7/TourScope",
-    // liveDemo: "",
-  },
-  {
-    id: 2,
-    title: "Clothes Store",
-    img: "/assets/clothesStore.png",
-    desc: "Developed a modern e-commerce web application using React and React Router. The project includes dynamic routing, a reusable component-based architecture, and a responsive design. Key features include a homepage, product listing, product detail",
-    githup: "https://github.com/abdelrahmanmostafa7/Clothes-Store",
-    liveDemo: "https://clothes-store-sandy-nine.vercel.app/",
+    id: 3,
+    title: "Nike Store",
+    img: "/assets/nikeStore.png",
+    desc: "Developed a responsive e-commerce web application for showcasing and selling Nike products using React, Redux Toolkit, and TailwindCSS. Implemented features like a dynamic shopping cart, product filtering, and interactive UI components to enhance user experience.",
+    githup: "https://github.com/abdelrahmanmostafa7/NikeStore",
+    liveDemo: "https://nike-store-lemon.vercel.app/",
   },
   {
     id: 5,
@@ -31,7 +15,6 @@ const ProjectsData = [
     githup: "https://github.com/abdelrahmanmostafa7/ChatApp-firebase",
     liveDemo: "https://chatapp-ashen-zeta.vercel.app/",
   },
-
   {
     id: 6,
     title: "Fitness App",
@@ -40,14 +23,33 @@ const ProjectsData = [
     githup: "https://github.com/abdelrahmanmostafa7/fitnessApp",
     liveDemo: "https://fitness-app-two-pink.vercel.app/",
   },
+  
   {
-    id: 3,
-    title: "Nike Store",
-    img: "/assets/nikeStore.png",
-    desc: "Developed a responsive e-commerce web application for showcasing and selling Nike products using React, Redux Toolkit, and TailwindCSS. Implemented features like a dynamic shopping cart, product filtering, and interactive UI components to enhance user experience.",
-    githup: "https://github.com/abdelrahmanmostafa7/NikeStore",
-    liveDemo: "https://nike-store-lemon.vercel.app/",
+    id: 4,
+    title: "Tour Scope",
+    img: "/assets/tourScope.png",
+    desc: "A full-stack MERN application designed for personalized travel planning. Features include AI-powered hotel and city recommendations, an interactive Mapbox map for exploration, responsive carousels for showcasing hotels, and a user-friendly interface. Integrated with secure authentication, dynamic data fetching, and a scalable backend API to deliver a seamless travel experience.",
+    githup: "https://github.com/abdelrahmanmostafa7/TourScope",
+    // liveDemo: "",
+  },
+  {
+    id: 1,
+    title: "Full Stack E-Commerce",
+    img: "/assets/ecommerce.png",
+    desc: "Developed a full-stack e-commerce platform with a responsive React frontend and a Node.js/Express backend. Implemented features like user authentication, product management, shopping cart functionality, and order processing. Utilized MongoDB for data storage and Redux for state management, ensuring a seamless user experience.",
+    githup: "https://github.com/abdelrahmanmostafa7/Full-Stack-E-Commerce",
+    liveDemo: "https://full-stack-e-commerce-neon.vercel.app/",
   },
 ];
 
 export default ProjectsData;
+
+
+ // {
+  //   id: 2,
+  //   title: "Clothes Store",
+  //   img: "/assets/clothesStore.png",
+  //   desc: "Developed a modern e-commerce web application using React and React Router. The project includes dynamic routing, a reusable component-based architecture, and a responsive design. Key features include a homepage, product listing, product detail",
+  //   githup: "https://github.com/abdelrahmanmostafa7/Clothes-Store",
+  //   liveDemo: "https://clothes-store-sandy-nine.vercel.app/",
+  // },
